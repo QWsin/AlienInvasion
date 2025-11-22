5ecf23bfafd88b4a6922b9724614f234d9fb0305
qwsin <2605162277@qq.com>
2025-11-22 21:12:59 +0800

    完成左右移动
    下一步设置移动速度可以更改，设置左右显移动限制

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (51, 153, 255)  # light gray
+        self.fps = 60
