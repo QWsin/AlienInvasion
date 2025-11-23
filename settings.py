d126c12fe6a40f710d6c63b5dd407ffe515d4f32
qwsin <2605162277@qq.com>
2025-11-23 08:02:19 +0800

    增加全屏模式，q退出，浮点飞船速度

--- settings.py
+++ settings.py
@@ -4,3 +4,13 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (51, 153, 255)  # light gray
         self.fps = 60
+        self.mode='windowed'
+
+        # ship settings
+        self.ship_speed = 6
+
+        # bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
--- ship.py
+++ ship.py
@@ -2,6 +2,7 @@ import pygame
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/spiked_ship_3_small.png')
@@ -11,11 +12,14 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
 
+        self.x = float(self.rect.x)
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
